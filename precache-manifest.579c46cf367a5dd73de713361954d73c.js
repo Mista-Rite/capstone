f1c278711a685435a3a0eb7976fb322dda4f3afd
Mista-Rite <ukaemmanueluche@gmail.com>
2020-11-23 12:22:42 +0100

    Updates

---
+++ precache-manifest.579c46cf367a5dd73de713361954d73c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "93a50a226c62b0765ac3",
+    "url": "/capstone/static/css/main.0a397d23.chunk.css"
+  },
+  {
+    "revision": "93a50a226c62b0765ac3",
+    "url": "/capstone/static/js/main.93a50a22.chunk.js"
+  },
+  {
+    "revision": "88eebe6e34865335e902",
+    "url": "/capstone/static/css/1.091ce370.chunk.css"
+  },
+  {
+    "revision": "88eebe6e34865335e902",
+    "url": "/capstone/static/js/1.88eebe6e.chunk.js"
+  },
+  {
+    "revision": "64b0c223d3f18225ab3a",
+    "url": "/capstone/static/js/runtime~main.64b0c223.js"
+  },
+  {
+    "revision": "eb25e12325808fc02ce87bb67174f8b4",
+    "url": "/capstone/static/media/projecticon.eb25e123.ico"
+  },
+  {
+    "revision": "c4bb620d1463c972ee3720c6a8f84ac3",
+    "url": "/capstone/index.html"
+  }
+];
